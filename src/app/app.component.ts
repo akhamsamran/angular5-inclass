f7e9348ace03d25f2f08841e72989ac22ee78d61
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-27 10:03:31 -0700

    added app/app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
 	selector: "angular5-example-app",
-	templateUrl: "./templates/angular5-example-app.html"
+	template: require("app.component.html")
 })
 
 export class AppComponent {}
